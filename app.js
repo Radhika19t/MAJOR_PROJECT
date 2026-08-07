03f939fd20a7a72fe084fb8b10fe66307030555a
Radhika Thakare <radhikathakare570@gmail.com>
2026-08-07 19:32:43 +0530

    Completed WanderLust project

--- app.js
+++ app.js
@@ -85,6 +85,11 @@ app.get("/", (req, res) => {
 
 
 
-app.listen(8080, () => {
-    console.log("Server is running on port 8080");
+// app.listen(8080, () => {
+//     console.log("Server is running on port 8080");
+// });
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
